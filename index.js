6fc66c8280b17ba094f8653647259d8ad1cd2bfe
AriewlMarcelino <paulo.estudante9904@gmail.com>
2022-12-04 15:41:58 -0300

    Adicionando resolução final do projeto, tanto em mobile como desktop

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
-const express = require('express'); 
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = 3000;
-const app =  express();
+const app = express();
 
-app.use(express.static('public'))
-const basePath = path.join(__dirname,"./public/templates");
+app.use(express.static("public"));
+const basePath = path.join(__dirname, "./public/templates");
 
+app.get("/home.html", (req, res) => {
+  res.sendFile(`${basePath}/home.html`);
+});
 
-app.get('/home.html',(req,res)=>{
-    res.sendFile(`${basePath}/home.html`)
-})
+app.get("/portifolio.html", (req, res) => {
+  res.sendFile(`${basePath}/Portifolio.html`);
+});
 
-app.get('/portifolio.html',(req,res)=>{
-    res.sendFile(`${basePath}/Portifolio.html`)
-})
+app.get("/sobre.html", (req, res) => {
+  res.sendFile(`${basePath}/sobre.html`);
+});
 
-app.get('/sobre.html',(req,res)=>{
-    res.sendFile(`${basePath}/sobre.html`)
-})
+app.get("/livros.html", (req, res) => {
+  res.sendFile(`${basePath}/livros.html`);
+});
 
+app.get("/blog.html", (req, res) => {
+  res.sendFile(`${basePath}/blog.html`);
+});
 
-app.get('/livros.html',(req,res)=>{
-    res.sendFile(`${basePath}/livros.html`)
-})
-
-
-app.get('/blog.html',(req,res)=>{
-    res.sendFile(`${basePath}/blog.html`)
-})
-
-
-app.get('/contato.html',(req,res)=>{
-    res.sendFile(`${basePath}/contato.html`)
-})
-
+app.get("/contato.html", (req, res) => {
+  res.sendFile(`${basePath}/contato.html`);
+});
 
 //middleware page 404
-app.use(function(req,res,next){
-    res.status(404).sendFile(`${basePath}/404.html`)
-})
+app.use(function (req, res, next) {
+  res.status(404).sendFile(`${basePath}/404.html`);
+});
 
-
-app.listen(port,()=>{
-    console.log(`Server is running port${port}`)
-})
-
-
+app.listen(port, () => {
+  console.log(`Server is running port${port}`);
+});
